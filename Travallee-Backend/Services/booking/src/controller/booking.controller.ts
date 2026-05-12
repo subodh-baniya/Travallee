1603cd428bf2d3908b4e716b38bf84a31ab36d34
Prabin K.C. <kcprabin2063@gmail.com>
2026-05-12 13:04:12 +0545

    booking controllers

--- Travallee-Backend/Services/booking/src/controller/booking.controller.ts
+++ Travallee-Backend/Services/booking/src/controller/booking.controller.ts
@@ -1,168 +1,83 @@
+//@ts-ignore
 import { asyncHandler, apiError, apiResponse, roomModel, hotelModel, bookingModel } from "@packages"
 import mongoose from "mongoose";
 import * as crypto from "crypto";
 import axios from "axios"
 
 
-// use this for import 
-//@ts-ignore
-import {sendEmail} from "@packages"
-// definitation of packages is in tsconfig.json file
+const taxrate = 13
 
+const calulateTotalAmount = (roomPrice: number, checkIn: Date, checkOut: Date ,guestCount: number) => {
+    const oneDay = 24 * 60 * 60 * 1000;
+    const nights = Math.round(Math.abs((checkOut.getTime() - checkIn.getTime()) / oneDay));
+    return roomPrice * nights ;
+}   
+
+const discountAmount = (totalAmount: number, discountPercentage: number) => {
+    return totalAmount * (discountPercentage / 100);
+}
+
+const taxAmount = (totalAmount: number, taxRate: number) => {
+    return totalAmount * (taxRate / 100);
+}   
 const createBooking = asyncHandler(async (req: any, res: any) => {
-
-    const session = await mongoose.startSession();
     try {
-        await session.withTransaction(async () => {
-            const { roomId, guests, checkIn, checkOut, paymentMethod } = req.body;
-            const start = new Date(checkIn);
-            const end = new Date(checkOut);
+        const { roomId,hotelId, checkIn, checkOut, guestCount } = req.body;
 
-            if (start >= end) {
-                throw new Error("Invalid entry of dates");
-            }
+        if (!roomId || !hotelId || !checkIn || !checkOut || !guestCount) {
+            return apiError(res, "All fields are required", 400);
+        }
 
-            const room = await roomModel
-                .findById(roomId)
-                .session(session);
+        const room = await roomModel.findById(roomId).populate("hotel");
+        const hotelDiscount = await hotelModel.findById(hotelId).select("discount");
+        if (!room) {
+            return apiError(res, "Room not found", 404);
+        }
 
-            if (!room) {
-                throw new Error("Room not found");
-            }
+        const hotel = await hotelModel.findById(hotelId);
+        if (!hotel) {
+            return apiError(res, "Hotel not found", 404);
+        }
 
-            if (room.capacity < guests) {
-                throw new Error("Room capacity exceeded");
-            }
+       const Checkavailability = await bookingModel.find({
+            room: roomId,
+            hotel: hotelId,
+            status: { $in: ["PENDING", "CONFIRMED"] },
+            $or: [
+                { checkIn: { $lt: new Date(checkOut), $gte: new Date(checkIn) } },
+                { checkOut: { $gt: new Date(checkIn), $lte: new Date(checkOut) } },
+                { checkIn: { $lte: new Date(checkIn) }, checkOut: { $gte: new Date(checkOut) } }
+            ]
+       })
 
-            const existingBooking = await bookingModel
-                .findOne({
-                    room: roomId,
-                    checkIn: { $lt: end },
-                    checkOut: { $gt: start },
-                    status: { $in: ["PENDING", "CONFIRMED"] }
-                })
-                .session(session);
+        if (Checkavailability.length >= room.quantity) {
+            return apiError(res, "Room is fully booked for the selected dates", 400);
+        }   
 
-            if (existingBooking) {
-                throw new Error("Room not available");
-            }
+        const totalAmount = calulateTotalAmount(room.price, new Date(checkIn), new Date(checkOut), guestCount);
+        const discount = discountAmount(totalAmount, hotelDiscount.discount);
+        const tax = taxAmount(totalAmount - discount, taxrate);
+        const finalAmount = totalAmount - discount + tax;
 
-            const nights =
-                (end.getTime() - start.getTime()) /
-                (1000 * 60 * 60 * 24);
-
-            const totalPrice = nights * room.pricePerNights;
-
-            const booking = new bookingModel({
-                user: req.user._id,
-                hotel: room.hotel,
-                room: roomId,
-                guests,
-                checkIn: start,
-                checkOut: end,
-                status: "PENDING",
-                bookingPayment: "NOTPAID",
-                totalPrice,
-                paymentMethod
-            });
-
-            await booking.save({ session });
-
-            const hotel = await hotelModel
-                .findById(room.hotel)
-                .session(session);
-
-            if (paymentMethod === "ESEWA") {
-
-                if (!hotel?.esewa_Merchantid) {
-                    throw new Error("Hotel doesnot have merchant id");
-                }
-
-                const transaction_uuid = booking._id.toString();
-                const product_code = hotel.esewa_Merchantid;
-                const total_amount = totalPrice.toString();
-
-                const signed_field_names =
-                    "total_amount,transaction_uuid,product_code";
-
-                const message =
-                    `total_amount=${total_amount},transaction_uuid=${transaction_uuid},product_code=${product_code}`;
-
-                const signature = crypto
-                    .createHmac("sha256", process.env.ESEWA_HASH_SECRET!)
-                    .update(message)
-                    .digest("base64");
-
-                const htmlForm = `<html>
-                 <body onload="document.forms[0].submit()">
-                 <form action="https://rc-epay.esewa.com.np/api/epay/main/v2/form" method="POST">
-                 <input type="hidden" name="total_amount" value="${total_amount}" />
-                 <input type="hidden" name="transaction_uuid" value="${transaction_uuid}" />
-                 <input type="hidden" name="product_code" value="${product_code}" />
-                 <input type="hidden" name="signature" value="${signature}" />
-                 <input type="hidden" name="signed_field_names" value="${signed_field_names}" />
-                 <input type="hidden" name="success_url" value="${process.env.BASE_URL}/api/payment/esewa/success" />
-                 <input type="hidden" name="failure_url" value="${process.env.BASE_URL}/api/payment/esewa/failure" />
-                 </form>
-                 </body>
-                 </html>`;
-
-                return apiResponse({ bookingId: booking._id, htmlForm }, 200, true, "Booking created via eSewa");
-            }
-
-            if (paymentMethod === "KHALTI") {
-
-                if (!hotel?.khalti_SecretKey) {
-                    throw new Error("Can't get the khalti secret key");
-                }
-
-                const totalpaisa = totalPrice * 100;
-
-                const khaltires = await axios.post("https://dev.khalti.com/api/v2/epayment/initiate/",
-                    {
-                        return_url: `${process.env.BASE_URL}/api/payment/khalti/verify`,
-                        website_url: `${process.env.FRONTEND_URL}`,
-                        amount: totalpaisa,
-                        purchase_order_id: booking._id.toString(),
-                        purchase_order_name: `${hotel.hotelName}`,
-                        customer_info: {
-                            "name": req.user.Name,
-                            "email": req.user.email,
-                        }
-                    },
-                    {
-                        headers: {
-                            "Authorization": `Key ${hotel.khalti_SecretKey}`,
-                            "Content-Type": "application/json",
-                        }
-                    }
-                )
-
-                const { pidx, payment_url } = khaltires.data
-
-                await bookingModel.findByIdAndUpdate(booking._id,
-                    { khalti_pidx: pidx },
-                    { session }
-                )
-
-                return apiResponse({ bookingId: booking._id, payment_url }, 200, true, "Booking created via Khalti");
-            }
-
-            return res.json({
-                success: true,
-                booking
-            });
-
+        const booking = await bookingModel.create({
+            user: req.user._id,
+            hotel: room.hotel._id,
+            room: room._id,
+            checkIn: new Date(checkIn), 
+            checkOut: new Date(checkOut),
+            guestCount,
+            totalAmount: finalAmount,
+            discount,
+            tax,
+            bookingPayment: "PENDING",
+            status: "PENDING"
         });
 
+        return apiResponse(res, "Booking created successfully", { bookingId: booking._id });
     } catch (error: any) {
-        return apiError({}, 400, error.message);
-    } finally {
-        session.endSession();
+        return apiError(res, "Failed to create booking", 500);
     }
-});
-
-
+})  
 const esewaSuccess = asyncHandler(async (req: any, res: any) => {
     try {
         const { data } = req.query;
@@ -199,7 +114,7 @@ const esewaSuccess = asyncHandler(async (req: any, res: any) => {
         })
 
         return res.redirect(`${process.env.FRONTEND_URL}/payment-failure`);
-    } catch (error) {
+    } catch (error: any ) {
         return res.redirect(`${process.env.FRONTEND_URL}/payment-failure`);
     }
 })
@@ -253,4 +168,4 @@ const khaltiVerify = asyncHandler(async (req: any, res: any) => {
 
 })
 
-export { khaltiVerify, createBooking, esewaSuccess }
+export { khaltiVerify,esewaSuccess }
