d7cef5d81b463d3ae98d0ce400bec0da8e51277c
Prabin K.C. <kcprabin2063@gmail.com>
2026-05-13 10:33:58 +0545

    some intilization of sockets in the projects

--- Travallee-Backend/Services/chat/src/app.ts
+++ Travallee-Backend/Services/chat/src/app.ts
@@ -1,4 +1,25 @@
-import { Express } from "express";
+import express from 'express';
 import { Server } from "http";
 import { Server as SocketIOServer } from "socket.io";
 
+
+const app = express();
+
+const httpServer = new Server(app);
+const io = new SocketIOServer(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
+
+export default app;
