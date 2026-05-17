4a2b7eb70a44ceecfbd9a69349752c7769990a60
subodh-baniya <baniyasubodh07@gmail.com>
2026-05-17 08:47:24 +0545

    hotel apis modified

--- User-Frontend/Services/hotel.api.ts
+++ User-Frontend/Services/hotel.api.ts
@@ -1,9 +1,35 @@
 import { hotelClient } from "./httpclient/hotel.client";
 
-export const getHotelRegistered=hotelClient.post('/register').then(res=>res.data);
+export const registerHotel = (data: FormData) => {
+  return hotelClient
+    .post("/register", data, {
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+    .then(res => res.data);
+};
 
-export const getHotelInfo=hotelClient.get('getHotelInfo').then(res=>res.data);
+export const getHotelInfo=hotelClient.get('/my-hotel').then(res=>res.data);
 
-export const createRoom = hotelClient.post ('/room/:hotelId').then(res=>res.data);
+export const createRoom = (hotelId: string, data: FormData) => {
+  return hotelClient
+    .post(`/room/${hotelId}`, data, {
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+    .then(res => res.data);
+};
 
-export const deleteRoom= hotelClient.delete('/room/:hotelId/:roomId').then(res=>res.data)
+export const deleteRoom = (  hotelId: string,roomId: string,password: string) => {
+  return hotelClient
+    .delete(`/room/${hotelId}/${roomId}`, {
+      data: { password },
+    })
+    .then(res => res.data);
+};
+
+export const getRooms = (hotelId: string, page = 1, limit = 10) => {
+  return hotelClient
+    .get(`/rooms/${hotelId}`, {
+      params: { page, limit },
+    })
+    .then(res => res.data);
+};
