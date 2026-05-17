b67ac5aec25f2e2eb23a068fa0cd806b891e274d
subodh-baniya <baniyasubodh07@gmail.com>
2026-05-17 07:25:37 +0545

    added some apis route in hotel.api

--- User-Frontend/Services/hotel.api.ts
+++ User-Frontend/Services/hotel.api.ts
@@ -3,3 +3,7 @@ import { hotelClient } from "./httpclient/hotel.client";
 export const getHotelRegistered=hotelClient.post('/register').then(res=>res.data);
 
 export const getHotelInfo=hotelClient.get('getHotelInfo').then(res=>res.data);
+
+export const createRoom = hotelClient.post ('/room/:hotelId').then(res=>res.data);
+
+export const deleteRoom= hotelClient.delete('/room/:hotelId/:roomId').then(res=>res.data)
