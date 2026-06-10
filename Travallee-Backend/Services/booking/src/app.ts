2b1e2924bad89b07673931380f48d332f1b2b6d7
subodh-baniya <baniyasubodh07@gmail.com>
2026-06-10 16:05:09 +0545

    fixed cors error

--- Travallee-Backend/Services/booking/src/app.ts
+++ Travallee-Backend/Services/booking/src/app.ts
@@ -7,7 +7,8 @@ const app=express();
 app.use(cors({
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 
 app.use(express.json({
