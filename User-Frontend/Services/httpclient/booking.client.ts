272a1515a52b3adaacbd714fa671beba09f634c2
kcprabin <kcprabin2063@gmail.com>
2026-06-25 10:32:27 +0545

    lol

--- User-Frontend/Services/httpclient/booking.client.ts
+++ User-Frontend/Services/httpclient/booking.client.ts
@@ -20,3 +20,14 @@ bookingClient.interceptors.request.use( (config) => {
   }, (error) => {
     return Promise.reject(error);
   } )
+
+bookingAdmin.interceptors.request.use( (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  }, (error) => {
+    return Promise.reject(error);
+  } )
+  
