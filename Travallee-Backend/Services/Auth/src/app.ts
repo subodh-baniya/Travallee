737fcee71f7200185bd4c4018e1c98861437daea
subodh-baniya <baniyasubodh07@gmail.com>
2026-06-25 23:59:46 +0545

    test debug

--- Travallee-Backend/Services/Auth/src/app.ts
+++ Travallee-Backend/Services/Auth/src/app.ts
@@ -33,6 +33,7 @@ app.use(express.static("public"));
 app.use(cookie());
 app.use(passport.initialize());
 app.get("/health", (req, res) => res.status(200).json({ status: "ok" }));
+app.get("/cors-check", (req, res) => res.json({ v: "options-fix-deployed" }));
 
 // user routes
 app.use("/api/v1/users", UserRouter);
