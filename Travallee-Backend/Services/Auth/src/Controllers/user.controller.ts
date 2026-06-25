f0fe435af0b4e81436f7d9af88d701031381c747
kcprabin <kcprabin2063@gmail.com>
2026-06-25 10:00:17 +0545

    lol

--- Travallee-Backend/Services/Auth/src/Controllers/user.controller.ts
+++ Travallee-Backend/Services/Auth/src/Controllers/user.controller.ts
@@ -98,9 +98,6 @@ const registerUser = asyncHandler(async (req: any, res: any) => {
 
     const otp = Math.floor(1000 + Math.random() * 9000);
 
-    await redisClient.set(`otp:${validate.email}`, otp, "EX", 10 * 60);
-    await redisClient.set(`pendingUser:${validate.email}`, JSON.stringify(validate), "EX", 10 * 60);
-
     try {
       await otpQueue.add("SendOTP", {
         email: validate.email,
@@ -238,10 +235,6 @@ const logoutUser = asyncHandler(async (req: any, res: any) => {
   const token = req.token;
   res.clearCookie("token");
   res.setHeader("Authorization", "");
-
-  await redisClient.del(`token:${req.user.id}`);
-  tokenBlacklistRedis.set(`blacklist:${token || ""}`, "true", "EX", 60 * 60 * 24 * 10);
-
   return apiResponse(res, 200, true, "User logged out successfully");
 });
 
